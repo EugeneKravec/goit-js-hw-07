15d92b2311554d9ec253597b50a6e2f3802ecabe
Eugene Kravec <krava255@gmail.com>
2021-09-10 16:13:37 +0300

    07.02 - done

--- homework-07/src/js/task-02.js
+++ homework-07/src/js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const ingredientsById = document.getElementById('ingredients')
+
+// 1-й способ через reduce
+
+// const addIngredients = ingredients.reduce((acc, elem) => {
+//   return acc + `<li>${elem}</li>`
+// }, '')
+// ingredientsById.insertAdjacentHTML('beforeend', addIngredients)
+
+// 2-й способ через document.createElement
+const newElems = ingredients.map((e) => {
+  const ingredient = document.createElement('li')
+  ingredient.textContent = e
+  return ingredient
+})
+ingredientsById.append(...newElems)
+
+
+
+console.log(ingredientsById);
+
