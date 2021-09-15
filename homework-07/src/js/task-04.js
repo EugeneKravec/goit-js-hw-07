48ff3002f0c703197febd37cd536c3b3cc0f8cdd
Eugene Kravec <krava255@gmail.com>
2021-09-15 10:15:45 +0300

    counter done

--- homework-07/src/js/task-04.js
+++ homework-07/src/js/task-04.js
@@ -1,13 +1,26 @@
-const counterValue = parseInt(document.getElementById('value').innerText, 10)
-console.dir(counterValue);
+let counterValue = 0
+
+const value = document.getElementById('value')
+console.log(counterValue);
+console.log(value);
+
+const incrementByData = document.querySelector("[data-action = 'increment']")
+console.log(incrementByData);
 
 const decrementByData = document.querySelector("[data-action = 'decrement']")
 console.log(decrementByData);
 
+incrementByData.addEventListener('click', increment)
+decrementByData.addEventListener('click', decrement)
+
 function increment() {
-    return counterValue += 1
+    console.log('ok');
+    counterValue += 1
+    return value.innerText = counterValue
 }
 function decrement() {
-    return counterValue -= 1
+    console.log('ok');
+    counterValue -= 1
+    return value.innerText = counterValue
 }
 
